6f58eb3f18e507441b5b6193e7581669763ad589
Derek Stegelman <dstegelman@gmail.com>
2015-04-12 10:02:13 -0600

    Simplify managers.

--- site_notifications/managers.py
+++ site_notifications/managers.py
@@ -6,19 +6,14 @@ from django.db.models.query import QuerySet
 
 
 class NotificationQuerySet(QuerySet):
-    def active(self):
-        return self.filter(enabled=True)
 
     def active_notifications(self):
-        return self.filter(start_date__lte=datetime.now(), end_date__gte=datetime.now())
+        return self.filter(start_date__lte=datetime.now(), end_date__gte=datetime.now(), enabled=True)
 
 
 class NotificationManager(Manager):
     def get_queryset(self):  # Removed in django1.8
         return NotificationQuerySet(self.model, using=self._db)
 
-    def active(self):
-        return self.get_queryset().active()
-
     def active_notifications(self):
-        return self.get_queryset().active().active_notifications()
+        return self.get_queryset().active_notifications()
