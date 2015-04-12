be660106d38eb784272905c3192278a5e5e545ed
Derek Stegelman <dstegelman@gmail.com>
2015-04-11 22:01:49 -0600

    Simple tests.

--- site_notifications/tests/factories.py
+++ site_notifications/tests/factories.py
@@ -9,6 +9,8 @@ from site_notifications.models import Notification
 class NotificationFactory(factory.DjangoModelFactory):
     start_date = datetime.datetime.now()
     end_date = datetime.datetime.now() + datetime.timedelta(days=2)
+    enabled = True
+    message = 'Test message'
 
     class Meta:
         model = Notification
